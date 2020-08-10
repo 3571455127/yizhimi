17b3b076f2ea6ec978dc2f51683473fe301d4937
yangpeijian <3571455127@qq.com>
2020-08-10 11:11:49 +0800

    20200810更新意大利语，德语

--- Ainaphp/Lang/de/common.php
+++ Ainaphp/Lang/de/common.php
@@ -1,5 +1,22 @@
 <?php
 return array (
-  'HOME_FONT' => '网站首页',
+  'HOME_FONT' => 'Zuhause',
+  'Toggle_navigation' => 'Navigation umschalten',
+  'Intelligent_Simple' => 'Intelligent. Einfach.',
+  'Yizumi_devoted_tom' => 'Yizumi widmete sich der intelligenten und einfachen Herstellung und Verwaltung von Gummiteilen',
+  'YIZUMI' => 'YIZUMI',
+  'General_model' => 'Allgemeines Modell',
+  'SINCE_2002' => 'SEIT 2002',
+  'Special_Models' => 'Sonder Modelle',
+  'Welcome_to_YIZUMI' => 'Willkommen bei YIZUMI RUBBER MACHINERY CO., LTD.',
+  'Name' => 'Name',
+  'Your_Name' => 'Dein Name',
+  'Email' => 'Email',
+  'Your_Email' => 'Deine E-Mail',
+  'Company_name' => 'Name der Firma',
+  'Your_Company' => 'Ihr Unternehmen',
+  'Message' => 'Botschaft',
+  'Your_Message' => 'Deine Nachricht',
+  'Submit_Form' => 'Formular senden'
 );
 ?>
